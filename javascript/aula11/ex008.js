9658e4a0711f8a9f11b99d1a2bc4dc8f82792b38
Thiago de Almeida Correia Fernandes <110740750+thiagoalmeidafernandes@users.noreply.github.com>
2022-08-30 19:35:43 -0300

    Criado primeiros arquivos .JS
    País e velocidade if else

---
+++ javascript/aula11/ex008.js
@@ -0,0 +1,10 @@
+console.log("O console funcionou corretamente!")
+
+var vel = 60.01
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+console.log(`Dirija sempre usando o sinto de segurança.`)
+
+if(vel > 60){//Condição Simples
+    console.log(`Você ultrapassou a velocidade permitida.`)
+}
---
+++ javascript/aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'BRASIL'
+ 
+if(país == 'Brasil'){
+    console.log(`Você é Brasileiro!`)
+} else {
+    console.log(`Você é estrangeiro`)
+}
